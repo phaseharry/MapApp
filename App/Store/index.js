145c0029e38244d0f86a709ba4e05cce80b2cdc6
Harry C <harrychenhc@icloud.com>
2019-02-14 22:09:21 -0500

    connected auth reducer to store

--- App/Store/index.js
+++ App/Store/index.js
@@ -3,9 +3,11 @@ import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
 import restaurantsReducer from './reducers/restaurants'
+import authReducer from './reducers/auth'
 
 const reducer = combineReducers({
-  restaurants: restaurantsReducer
+  restaurants: restaurantsReducer,
+  auth: authReducer
 })
 
 const store = createStore(reducer, applyMiddleware(thunk, logger))
